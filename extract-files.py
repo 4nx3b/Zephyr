2096baaf68ed6878ef906c4d097983d0e1768748
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-03-15 23:16:51 +0000

    zephyr: Initial commit

---
+++ extract-files.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/oplus/zephyr',
+    'hardware/mediatek',
+    'hardware/mediatek/libmtkperf_client',
+    'hardware/oplus',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    ('libsink',): lib_fixup_remove,
+}
+
+
+blob_fixups: blob_fixups_user_type = {
+    'odm/bin/hw/android.hardware.ir-service': blob_fixup()
+        .replace_needed('android.hardware.ir-V1-ndk_platform.so', 'android.hardware.ir-V1-ndk.so'),
+    'odm/lib64/liblvimfs_wrapper.so': blob_fixup()
+        .remove_needed('libstdc++.so'),
+    'odm/lib64/vendor.oplus.hardware.hdcp-V1-ndk_platform.so': blob_fixup()
+        .replace_needed('android.hardware.common-V2-ndk_platform.so', 'android.hardware.common-V2-ndk.so'),
+    'vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc': blob_fixup()
+        .regex_replace('start', 'enable'),
+    'system_ext/lib64/libsink-mtk.so': blob_fixup()
+        .add_needed('libaudioclient_mt6895_shim.so'),
+    'system_ext/lib64/libimsma.so': blob_fixup()
+        .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+    ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
+        .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
+    'vendor/bin/hw/android.hardware.security.keymint-service.trustonic': blob_fixup()
+        .add_needed('android.hardware.security.rkp-V3-ndk.so')
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so'),
+    'vendor/bin/hw/mtkfusionrild' : blob_fixup()
+        .add_needed('libutils-v32.so'),
+    ('vendor/bin/mnld', 'vendor/lib64/liboplus_mtkcam_lightsensorprovider.so', 'vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so', 'vendor/lib64/mt6895/libaalservice.so'): blob_fixup()
+        .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
+    'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so'),
+    ('vendor/lib64/mt6895/libcam.hal3a.so',
+     'vendor/lib64/mt6895/libcam.hal3a.ctrl.so',
+     'vendor/lib64/mt6895/libmtkcam_request_requlator.so',
+     'vendor/lib64/libcustomer_cameradata.so'): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
+    'vendor/lib64/hw/hwcomposer.mtk_common.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
+    'vendor/lib64/hw/mt6895/android.hardware.camera.provider@2.6-impl-mediatek.so': blob_fixup()
+        .add_needed('libcamera_metadata_shim.so')
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+    'vendor/lib64/hw/mt6895/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
+        .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so') 
+        .binary_regex_replace(b'/my_product/vendor/etc/cust_silky_brightness_%s_%s.xml', b'/vendor/etc/cust_silky_brightness_%s_%s.xml\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+        .binary_regex_replace(b'/my_product/vendor/etc/cust_silky_brightness_%s.xml', b'/vendor/etc/cust_silky_brightness_%s.xml\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+    ('vendor/lib64/lib3a.ae.pipe.so', 'vendor/lib64/mt6895/lib3a.awbsync.so', 'vendor/lib64/mt6895/lib3a.flash.so',
+    'vendor/lib64/mt6895/lib3a.sensors.color.so', 'vendor/lib64/mt6895/lib3a.sensors.flicker.so'): blob_fixup()
+        .add_needed('liblog.so'),
+    'vendor/lib64/libmidasserviceintf_aidl.so': blob_fixup()
+        .replace_needed('android.frameworks.stats-V1-ndk_platform.so', 'android.frameworks.stats-V1-ndk.so'),
+    'vendor/lib64/mt6895/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_createFromHandle')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock')
+        .add_needed('libbase_shim.so'),
+    ('vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
+        .add_needed('libbase_shim.so'),
+    'vendor/lib64/mt6895/libmnl.so': blob_fixup()
+        .add_needed('libcutils.so'),
+    'vendor/lib64/mt6895/libmtkcam_stdutils.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'zephyr',
+    'oplus',
+    add_firmware_proprietary_file=True,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
---
+++ lineage_zephyr.mk
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/oplus/zephyr/device.mk)
+
+# Inherit from the LineageOS configuration.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := Oplus
+PRODUCT_DEVICE := zephyr
+PRODUCT_MANUFACTURER := Oplus
+PRODUCT_MODEL := PGKM10
+PRODUCT_NAME := lineage_zephyr
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="sys_mssi_64_cn_armv82-user 15 AP3A.240617.008 1755697740151 release-keys" \
+    BuildFingerprint=OnePlus/PGKM10/OP5565:15/AP3A.240617.008/S.1f56c75-1-a469:user/release-keys \
+    DeviceName=PGKM10 \
+    DeviceProduct=PGKM10 \
+    SystemDevice=PGKM10 \
+    SystemName=PGKM10
+
