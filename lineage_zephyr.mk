981160184da934cdc8e5e9a713a55b19e4586858
yuki <asaxxhiii@gmail.com>
2026-07-10 00:51:59 +0530

    Add custom flags and optimizations to lineage_zephyr.mk

--- lineage_zephyr.mk
+++ lineage_zephyr.mk
@@ -22,3 +22,29 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=OnePlus/PGKM10/OP5565:15/AP3A.240617.008/S.1f56c75-1-a469:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
+# Lunaris Flags
+TARGET_CUSTOM_UDFPS := true
+WITH_GMS := true
+WITH_GMS_COMMS_SUITE := false
+WITH_PIXEL_LAUNCHER := true
+TARGET_USE_WALLPAPERS := true
+TARGET_USE_GPHOTOS := false
+TARGET_USE_FILES := false
+TARGET_USE_MAPS := false
+USE_REALITY_ENGINE := false
+$(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
+SURFACE_FLINGER_BOOST := true
+TARGET_DISABLE_MATLOG:= true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_WEATHER := false
+WITH_BCR := false
+
+
+# Optimisations 
+TARGET_OPTIMIZED_DEXOPT := true
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed
+WITH_DEXPREOPT := true
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
