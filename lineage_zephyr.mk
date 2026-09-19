fc987a3b9d804224a2102b50c43a337678eec4b7
yuki <asaxxhiii@gmail.com>
2026-09-19 23:02:02 +0530

    Update lineage_zephyr.mk with new configurations
    Added camera information, maintainer, and processor details.

--- lineage_zephyr.mk
+++ lineage_zephyr.mk
@@ -22,3 +22,21 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=OnePlus/PGKM10/OP5565:15/AP3A.240617.008/S.1f56c75-1-a469:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
+#Assion Shit 
+TARGET_INCLUDE_AXFX := true
+
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50,8,2
+AXION_CAMERA_FRONT_INFO := 32
+
+# Maintainer name
+AXION_MAINTAINER := v3rsi0n
+
+# Processor name
+AXION_PROCESSOR := Dimenshitty_8100_Max
+
+#Extras 
+TARGET_INCLUDE_AXFX := true
+TARGET_INCLUDES_LOS_PREBUILTS := false
+axion panther core
